b43199f2dc8adb4b8714e62438718d2afd33c032
begizew <begizewzerihun@gmail.com>
2024-12-10 10:57:22 -0500

    start to make more atractive

--- app/page.tsx
+++ app/page.tsx
@@ -29,18 +29,22 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+              width={560}
+              height={620}
+              className="block md:hidden"
+              alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
-      </div>
-      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-      {/* Add Hero Images Here */}
-      <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+
       </div>
     </main>
   );
